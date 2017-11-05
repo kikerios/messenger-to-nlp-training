2a419d4c6bdb972fcdd02b0c4bdbd6d83d87fd81
Kike Rios <web.lrios@gmail.com>
2017-11-05 17:26:12 -0600

    refactor resolvers

--- server/qraphql/resolvers.js
+++ server/qraphql/resolvers.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const config = require('../../config')
+let rdb = null
 
 const Promise = require('bluebird'),
   Logger = require('bucker').createLogger({
@@ -8,81 +9,83 @@ const Promise = require('bluebird'),
     console: config.get('/logger/options/console')
   })
 
-let rdb
+const rethinkdbHelper = require('../lib/rethinkdb-helper')()
+  .then(rethinkdb => {
+    rdb = rethinkdb
+  }),
+  db = () => {
+    if (!rdb)
+      throw ('rethinkdb - connection')
+    return rdb
+  }
 
-const rethinkdbHelper = require('../lib/rethinkdb-helper')().then(rethinkdb => {
-  rdb = rethinkdb
-})
+const Query = {},
+  Mutation = {}
 
-const db = () => {
-  if (!rdb)
-    throw ('rethinkdb - connection')
-  return rdb
+Query.getPages = (root) => {
+  Logger.info('getPages')
+  const {r, conn} = db()
+  return new Promise((resolve, reject) => {
+    r.table('pages').filter({}).run(conn, (err, results) => {
+      if (err)
+        reject(err)
+      else
+        resolve(results.toArray())
+    })
+  })
+}
+
+Query.getPageById = (root, args) => {
+  Logger.info('getPageById %j', args)
+  const {r, conn} = db()
+  return new Promise((resolve, reject) => {
+    r.table('pages').get(args.id).run(conn, (err, result) => {
+      if (err)
+        reject(err)
+      else
+        resolve(result)
+    })
+  })
+}
+
+Mutation.createPage = (root, args) => {
+  const {r, conn} = db()
+  Logger.info('createPage %j', args)
+  return new Promise((resolve, reject) => {
+    r.table('pages').get(args.id).replace(args).run(conn, (err, result) => {
+      if (err)
+        reject(err)
+      else
+        resolve(args)
+    })
+  })
+}
+
+Mutation.createPages = (root, args) => {
+  Logger.info('createPages %j', args)
+  const {r, conn} = db()
+  const res = []
+  return new Promise(resolve => {
+    return Promise.reduce(args.pages, (res, page) => {
+      return new Promise((resolve, reject) => {
+        Logger.info('createPages -> %j', page)
+        r.table('pages').get(page.id).replace(page).run(conn, (err, result) => {
+          if (err) {
+            reject(res)
+          } else {
+            res.push(page)
+            resolve(res)
+          }
+        })
+      })
+    }, res).then(results => {
+      Logger.info('createPages -> results -> %j', results)
+      resolve(results)
+    })
+  })
 }
 
 module.exports = () => ({
-  Query: {
-    getPages(root) {
-      Logger.info('getPages')
-      const {r, conn} = db()
-      return new Promise((resolve, reject) => {
-        r.table('pages').filter({}).run(conn, (err, results) => {
-          if (err)
-            reject(err)
-          else
-            resolve(results.toArray())
-        })
-      })
-    },
-    getPageById(root, args) {
-      Logger.info('getPageById %j', args)
-      const {r, conn} = db()
-      return new Promise((resolve, reject) => {
-        r.table('pages').get(args.id).run(conn, (err, result) => {
-          if (err)
-            reject(err)
-          else
-            resolve(result)
-        })
-      })
-    }
-  },
-  Mutation: {
-    createPage(root, args) {
-      const {r, conn} = db()
-      Logger.info('createPage %j', args)
-      return new Promise((resolve, reject) => {
-        r.table('pages').get(args.id).replace(args).run(conn, (err, result) => {
-          if (err)
-            reject(err)
-          else
-            resolve(args)
-        })
-      })
-    },
-    createPages(root, args) {
-      Logger.info('createPages %j', args)
-      const {r, conn} = db()
-      const res = []
-      return new Promise(resolve => {
-        return Promise.reduce(args.pages, (res, page) => {
-          return new Promise((resolve, reject) => {
-            Logger.info('createPages -> %j', page)
-            r.table('pages').get(page.id).replace(page).run(conn, (err, result) => {
-              if (err) {
-                reject(res)
-              } else {
-                res.push(page)
-                resolve(res)
-              }
-            })
-          })
-        }, res).then(results => {
-          Logger.info('createPages -> results -> %j', results)
-          resolve(results)
-        })
-      })
-    }
-  }
+  Query,
+  Mutation
 })
-
